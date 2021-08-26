b23e8c4dbc4cd205a4a6900e4f8acb2c9dbd01ff
Tarek Aziz <tarekiub17@gmail.com>
2021-08-26 18:40:14 +0600

    template added

--- spm/views.py
+++ spm/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
+posts = [
+    {
+        'uni_name': 'Independent University, Bangladesh(IUB)',
+        'year': '1993',
+        'type': 'Private University'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>SPM Home</h1>')
+    #return HttpResponse('<h1>SPM Home</h1>')
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'spm/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About SPM</h1>')
+    return render(request, 'spm/about.html', {'title': 'About'})
