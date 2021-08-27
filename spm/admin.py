da72a259aff5f3b1c5420e2c5472fe630329b1d8
Tarek Aziz <tarekiub17@gmail.com>
2021-08-28 05:08:13 +0600

    some table created

--- spm/admin.py
+++ spm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import School_T, Department_T, Program_T, Student_T
 # Register your models here.
+admin.site.register(School_T)
+admin.site.register(Department_T)
+admin.site.register(Program_T)
+admin.site.register(Student_T)
+
--- spm/models.py
+++ spm/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
-class school_T(models.Model):
+class School_T(models.Model):
     school_id = models.CharField(max_length= 5, primary_key=True)
     school_name = models.CharField(max_length= 30)
-    
+
+class Department_T(models.Model):
+    department_id = models.CharField(max_length= 10, primary_key=True)
+    department_name = models.CharField(max_length= 30)
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+
+class Program_T(models.Model):
+    program_id = models.CharField(max_length= 5, primary_key=True)
+    program_name = models.CharField(max_length= 30)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE, default=None)
+
+class Student_T(models.Model):
+    student_id = models.CharField(max_length= 8, primary_key=True)
+    f_name = models.CharField(max_length= 20)
+    l_name = models.CharField(max_length= 20)
+    email = models.EmailField(max_length = 30)
+    contact_no = models.CharField(max_length= 15)
+    gender = models.CharField(max_length= 6)
+    date_of_birth = models.DateTimeField()
+    street = models.CharField(max_length= 30)
+    city = models.CharField(max_length= 20)
+    state = models.CharField(max_length= 20)
+    year = models.CharField(max_length = 5)
+    semseter = models.CharField(max_length= 10)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE, default=None)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE, default=None)
+
+
