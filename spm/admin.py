ecd412a21a739d2f120b4ee378e88776e4dd6c24
Tarek Aziz <tarekiub17@gmail.com>
2021-08-28 05:46:15 +0600

    more table created

--- spm/admin.py
+++ spm/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import School_T, Department_T, Program_T, Student_T
+from .models import School_T, Department_T, Program_T, Student_T, Course_T, Pre_req_course_T, PLO_T, CO_T
 # Register your models here.
 admin.site.register(School_T)
 admin.site.register(Department_T)
 admin.site.register(Program_T)
 admin.site.register(Student_T)
-
+admin.site.register(Course_T)
+admin.site.register(Pre_req_course_T)
+admin.site.register(PLO_T)
+admin.site.register(CO_T)
--- spm/models.py
+++ spm/models.py
@@ -25,9 +25,32 @@ class Student_T(models.Model):
     street = models.CharField(max_length= 30)
     city = models.CharField(max_length= 20)
     state = models.CharField(max_length= 20)
-    year = models.CharField(max_length = 5)
+    year = models.IntegerField(name='Year of admission', null=True)
     semseter = models.CharField(max_length= 10)
     program = models.ForeignKey(Program_T, on_delete=models.CASCADE, default=None)
     department = models.ForeignKey(Department_T, on_delete=models.CASCADE, default=None)
+
+class Course_T(models.Model):
+    course_id = models.CharField(max_length= 6, primary_key=True)
+    course_name = models.CharField(max_length= 25)
+    course_type = models.CharField(max_length= 10)
+    no_of_credit = models.IntegerField(name='No of credits')
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+
+class Pre_req_course_T(models.Model):
+    course_id = models.CharField(max_length= 6, primary_key=True)
+    Pre_req_course_id = models.ForeignKey(Course_T, on_delete=models.CASCADE)
+
+class PLO_T(models.Model):
+    plo_id = models.CharField(max_length= 5, primary_key=True)
+    plo_name = models.CharField(max_length= 30)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+
+class CO_T(models.Model):
+    co_id = models.CharField(max_length= 5, primary_key=True)
+    details = models.TextField()
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE)
+    plo_id = models.ForeignKey(PLO_T, on_delete=models.CASCADE)
+
 
 
