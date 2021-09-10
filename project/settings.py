4623803514737bacf99dcd56643e4f5919367a74
Tarek Aziz <tarekiub17@gmail.com>
2021-09-10 16:38:32 +0600

    user registration

--- project/settings.py
+++ project/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'spm.apps.SpmConfig',
-    'import_export',
     'users.apps.UsersConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +135,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
